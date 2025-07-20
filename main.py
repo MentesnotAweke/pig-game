46f0c0f07e2c12338900139fbfe2cd4404cb412d
mintesnot <mentesnotaa@gmail.com>
2025-07-20 03:08:33 +0300

    Add turn-based game loop with score tracking and dice roll logic

--- main.py
+++ main.py
@@ -16,5 +16,29 @@ while True:
     else:
         print("Please enter a number between 2 and 4.")
 
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print("\n Player number:", player_idx + 1 ,"turn has just starter \n")
+        current_score = 0
+        while True:
+            should_roll = input("Do you want to roll the dice? (y): ").strip().lower()
+            if should_roll != 'y':
+                break
+            value = roll_dice()
+            if value == 1:
+                print("You rolled a 1! Your turn is over, and you lose all points for this turn.")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print (f"You rolled a {value}. Your current score for this turn is {current_score}.")
+
+        player_scores[player_idx] += current_score
+        print(f"Player {player_idx + 1} total score: {player_scores[player_idx]}")
+
+print(player_scores)
 print(f"Number of players: {players}") 
  
